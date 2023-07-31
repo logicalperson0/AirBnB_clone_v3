b01cc514f09efcc32b96b595fc7806c2b96068ea
Samuel <a2jsamuel@gmail.com>
2023-08-01 01:42:46 +0300

    Undate app.py with cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 Script that imports a Blueprint(app_views) and runs Flask
 """
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -10,6 +11,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
