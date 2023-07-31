59a543cd273dfa9982a705eba68ed78e38ccbf6e
Godswill Utong <94043309+G-V-Utong@users.noreply.github.com>
2023-07-31 20:47:02 +0100

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,7 +3,8 @@
 route for handling State objects and operations
 """
 from flask import jsonify, abort, request
-from api.v1.views import app_views, storage
+from api.v1.views import app_views
+from models import storage
 from models.state import State
 
 
